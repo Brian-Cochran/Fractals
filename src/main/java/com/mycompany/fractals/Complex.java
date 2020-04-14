9ade6f80e027aa54b947cd5c03c93b759841b6c3
Brian Cochran <bcbriancochran@gmail.com>
2020-04-13 23:10:56 -0500

    feat: Add multiply, magnitude, and add methods

--- src/main/java/com/mycompany/fractals/Complex.java
+++ src/main/java/com/mycompany/fractals/Complex.java
@@ -11,11 +11,35 @@ package com.mycompany.fractals;
  */
 public class Complex {
     
-    public double real;
-    public double imaginary;
+    public static double real;
+    public static double imaginary;
     
     public Complex(double r, double i) {
         this.real = r;
         this.imaginary = i;
     } // Complex(double, double)
+    
+    public double getReal() {
+        return this.real;
+    } // getReal()
+    
+    public double getImaginary() {
+        return this.imaginary;
+    } // getImaginary()
+    
+    public Complex add(Complex c) {
+        double newReal = this.getReal() + c.getReal();
+        double newImaginary = this.getImaginary() + c.getImaginary();
+        return new Complex(newReal, newImaginary);
+    } // add(Complex)
+    
+    public Complex multiply(Complex c) {
+        double newReal = (this.getReal() * c.getReal()) - (this.getImaginary() * c.getImaginary());
+        double newImaginary = (this.getReal() * c.getImaginary()) - (this.getImaginary() * c.getReal());
+        return new Complex(newReal, newImaginary);
+    } // multiply(Complex)
+    
+    public double magnitude() {
+        return Math.sqrt((this.getReal() * this.getReal()) + (this.getImaginary() * this.getImaginary()));
+    } // magnitude()
 } // Complex
