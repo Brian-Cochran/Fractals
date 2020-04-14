cfb46c8c03da35698727e460d0a8fae66cecccc5
Brian Cochran <bcbriancochran@gmail.com>
2020-04-13 22:52:45 -0500

    feat: Create class and add constructor

---
+++ src/main/java/com/mycompany/fractals/Complex.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fractals;
+
+/**
+ *
+ * @author Brian Cochran
+ */
+public class Complex {
+    
+    public double real;
+    public double imaginary;
+    
+    public Complex(double r, double i) {
+        this.real = r;
+        this.imaginary = i;
+    } // Complex(double, double)
+} // Complex
