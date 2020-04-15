ceccda9a6cc1dd29456ae33250033f7d1d7d0820
Brian Cochran <bcbriancochran@gmail.com>
2020-04-15 01:37:14 -0500

    Edits to SimpleBitMap class

--- src/main/java/com/mycompany/fractals/SimpleBitMap.java
+++ src/main/java/com/mycompany/fractals/SimpleBitMap.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.fractals;
 
 import java.awt.Graphics;
@@ -12,32 +8,59 @@ import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import javax.swing.JPanel;
 
-/**
- *
- * @author Brian Cochran
- */
 public class SimpleBitMap extends JPanel {
-    
+
     private static final int BITMAP_WIDTH = 512;
     private static final int BITMAP_HEIGHT = 512;
     private BufferedImage image;
+
+    private final double minX = 0;
+    private final double maxX = BITMAP_WIDTH - 1;
+    private final double minY = 0;
+    private final double maxY = BITMAP_HEIGHT - 1;    
+
+    private final double minU = -1.3;
+    private final double maxU = -0.2;
+    private final double minV = -1.3;
+    private final double maxV = -0.2;
+
+    private final int MAX_ITERATIONS = 100;
+    private final double MAGNITUDE = 4.0;
     
     public SimpleBitMap() {
         int w = BITMAP_WIDTH;
         int h = BITMAP_HEIGHT;
         int imageType = BufferedImage.TYPE_INT_RGB;
         this.image = new BufferedImage(w, h, imageType);
-    } // SimpleBitMap()
+    }// SimpleBitMap()
     
-    public int getWidth() {
-        return this.BITMAP_WIDTH;
-    } // getWidth()
+    public Complex mapping(double x, double y) {
+        double u = minU + (maxU - minU) * ((x - minX) / (maxX - minX));
+        double v = minV + (maxV - minV) * ((y - minY) / (maxY - minY));
+        return new Complex(u, v);
+    }// mapping(Complex)
+
+    public int checkPoint(Complex c) {
+        Complex z = new Complex(0, 0);
+        int count = -1;
+        while(z.magnitudeSquared() < MAGNITUDE && count < MAX_ITERATIONS) {
+            z = c.add(z.multiply(z));
+            count++;
+        }// while
+        return count;
+    }// checkPoint(Complex c)
     
-    public int getHeight() {
-        return this.BITMAP_HEIGHT;
-    } // getHeight()
+    public int[] chooseColor(int value) {
+        if (value < 10) {
+            int[] color = {0, 0, 0};
+            return color;
+        } // if
+        else {
+            int[] color = {0, 255, 0};
+            return color;
+        } // else
+    } // chooseColor(int)
     
-    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
@@ -49,21 +72,15 @@ public class SimpleBitMap extends JPanel {
         scale.setToScale(((double) w) / BITMAP_WIDTH, ((double) h) / BITMAP_HEIGHT);
         
         WritableRaster raster = this.image.getRaster();
-        
-        int[] black = {0, 0, 0};
-        int[] yellow = {255, 255, 0};
-    
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (j < i) {
-                    raster.setPixel(j, i, yellow);
-                } // if
-                else {
-                    raster.setPixel(j, i, black);
-                } // else
-            } // for
-        } // for
-        
+         
+        for(int row = 0; row < h; row++) {
+            for(int column = 0; column < w; column++) {
+                Complex c = mapping(row, column);
+                int colorValue = checkPoint(c); 
+                int[] color = chooseColor(colorValue);
+                raster.setPixel(row, column, color);
+            }// for
+        }// for
         g2D.drawImage(image, scale, this);
-    } // paintComponent(Graphics)
+    }// paintComponent(Graphics)
 } // SimpleBitMap
