7b47c6b5138f0c3f4235425183354d654f12b153
Brian Cochran <bcbriancochran@gmail.com>
2020-04-15 01:19:05 -0500

    edits to Complex class methods

--- src/main/java/com/mycompany/fractals/Complex.java
+++ src/main/java/com/mycompany/fractals/Complex.java
@@ -1,45 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.fractals;
 
-/**
- *
- * @author Brian Cochran
- */
 public class Complex {
-    
-    public static double real;
-    public static double imaginary;
-    
+
+    private final double real;
+    private final double imaginary;
+
     public Complex(double r, double i) {
         this.real = r;
         this.imaginary = i;
-    } // Complex(double, double)
-    
+    }// Complex(double, double)
+
+    public Complex add(Complex c) {
+        double u = this.real + c.real;
+        double v = this.imaginary + c.imaginary;
+        
+        return new Complex(u, v);
+    }// add(Complex)
+
+    public Complex multiply(Complex c) {
+        double u = (this.real * c.real) - (this.imaginary * c.imaginary);
+        double v = (this.real * c.imaginary) + (c.real * this.imaginary);
+        return new Complex(u, v);
+    }// multiply(Complex)
+
+    public double magnitudeSquared() {
+        double rSquare = this.real * this.real;
+        double iSquare = this.imaginary * this.imaginary; 
+        return rSquare + iSquare;
+    }// magnitudeSquared()
+
     public double getReal() {
         return this.real;
-    } // getReal()
-    
+    }// getReal()
+
     public double getImaginary() {
         return this.imaginary;
-    } // getImaginary()
-    
-    public Complex add(Complex c) {
-        double newReal = this.getReal() + c.getReal();
-        double newImaginary = this.getImaginary() + c.getImaginary();
-        return new Complex(newReal, newImaginary);
-    } // add(Complex)
-    
-    public Complex multiply(Complex c) {
-        double newReal = (this.getReal() * c.getReal()) - (this.getImaginary() * c.getImaginary());
-        double newImaginary = (this.getReal() * c.getImaginary()) + (this.getImaginary() * c.getReal());
-        return new Complex(newReal, newImaginary);
-    } // multiply(Complex)
-    
-    public double magnitudeSqr() {
-        return ((this.getReal() * this.getReal()) + (this.getImaginary() * this.getImaginary()));
-    } // magnitudeSqr()
-} // Complex
+    }// getImaginary()
+}// Complex
