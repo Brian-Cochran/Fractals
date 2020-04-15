388b2e3deb0a8c440e9af9057c83a08f4d0cc89e
Brian Cochran <bcbriancochran@gmail.com>
2020-04-15 12:55:35 -0500

    Add javadoc comments

--- src/main/java/com/mycompany/fractals/Simple.java
+++ src/main/java/com/mycompany/fractals/Simple.java
@@ -4,12 +4,25 @@ package com.mycompany.fractals;
 import java.awt.Container;
 import javax.swing.JFrame;
 
+/**
+ * Creates a window to draw the Mandelbrot set on.
+ * 
+ * @author Brian Cochran
+ * @version 4/15/2020
+ */
 public class Simple extends JFrame {
 
     private static final int WINDOW_WIDTH = 512;
     private static final int WINDOW_HEIGHT = 512;
-    private static final String WINDOW_TITLE = "Mandlebrot";
-
+    private static final String WINDOW_TITLE = "Mandelbrot";
+    
+    /**
+     * Simple constructor.
+     * <p>
+     * Creates an instance of the Simple class with a specified width, height, 
+     * and title. It also creates an instance of the SimpleBitMap class to create 
+     * a panel.
+     */
     public Simple() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -20,7 +33,12 @@ public class Simple extends JFrame {
         pane.add(panel);
         this.setVisible(true);
     }// Simple()
-
+    
+    /**
+     * Creates a window with a panel containing a drawing of the Mandelbrot set.
+     * 
+     * @param args string array containing command-line arguments
+     */
     public static void main(String[] args) {
         Simple simple = new Simple();
     }// main(String[])
