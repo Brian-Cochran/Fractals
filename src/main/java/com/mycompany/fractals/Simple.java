8ed18cb822bd98f6cce6ade97f6376d912ffd71d
Brian Cochran <bcbriancochran@gmail.com>
2020-04-15 01:21:09 -0500

    Edits to Simple class

--- src/main/java/com/mycompany/fractals/Simple.java
+++ src/main/java/com/mycompany/fractals/Simple.java
@@ -1,44 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.fractals;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Container;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
-/**
- *
- * @author Brian Cochran
- */
 public class Simple extends JFrame {
-    
-    private static final int SIMPLE_WIDTH = 512;
-    private static final int SIMPLE_HEIGHT = 512;
-    private static final String SIMPLE_TITLE = "Simple";
-    
+
+    private static final int WINDOW_WIDTH = 512;
+    private static final int WINDOW_HEIGHT = 512;
+    private static final String WINDOW_TITLE = "Mandlebrot";
+
     public Simple() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(SIMPLE_WIDTH, SIMPLE_HEIGHT);
-        this.setTitle(SIMPLE_TITLE);
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setTitle(WINDOW_TITLE);
         
         Container pane = this.getContentPane();
-        
         SimpleBitMap panel = new SimpleBitMap();
         pane.add(panel);
-        
         this.setVisible(true);
-    } // Simple()
-    
-    /**
-     * @param args the command line arguments
-     */
+    }// Simple()
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    } // main(String[])
+        Simple simple = new Simple();
+    }// main(String[])
 } // Simple
